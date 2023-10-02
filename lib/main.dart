15a9ee32678f83f1e21c0e276f0488c3517ad6e4
Shougotmollik <shougotmollik@gmail.com>
2023-10-02 22:42:12 +0600

    updated the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_appliaction/pages/home_page.dart';
 
-void main() {
+void main() async {
+  // init hive
+  await Hive.initFlutter();
+  // open a box
+  var box = await Hive.openBox("mybox");
   runApp(const MyApp());
 }
 
