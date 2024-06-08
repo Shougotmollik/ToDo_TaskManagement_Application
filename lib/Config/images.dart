6c40c53c07fe13c1665765396963e6ebc74ddad3
Shougot mollik <shougotmollik@gmail.com>
2024-06-08 19:28:49 +0600

    images

---
+++ lib/Config/images.dart
@@ -0,0 +1,8 @@
+class AppImages {
+  AppImages._();
+
+  static const String bashPath = 'assets';
+  static const String imagePath = '$bashPath/images';
+
+  static const String bgImage = '$imagePath/background.svg';
+}
