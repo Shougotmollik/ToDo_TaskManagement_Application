d66cf9a30663f6568e419a9610442405ff816c28
Shougot mollik <shougotmollik@gmail.com>
2024-06-08 19:27:46 +0600

    app

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:todoapp/Screens/HomeScreen/home_screen.dart';
+import 'package:todoapp/Screens/AuthScreen/signup_screen.dart';
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData.light(),
+      home: const SignUpScreen(),
     );
   }
 }
