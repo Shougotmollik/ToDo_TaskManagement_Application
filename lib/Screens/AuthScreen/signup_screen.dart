483db4ec0b76b463d43fb7fc1bc90d8d44c3e84c
Shougot mollik <shougotmollik@gmail.com>
2024-06-08 19:29:06 +0600

    sign up screen

---
+++ lib/Screens/AuthScreen/signup_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp/Screens/widgets/background_widgets.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: BackgroundWidgets(
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
