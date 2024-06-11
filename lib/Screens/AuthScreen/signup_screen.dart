a1999cde4d78a5dd34094a7476e52a3ba47c81af
Shougot mollik <shougotmollik@gmail.com>
2024-06-12 00:55:52 +0600

    signup screen

--- lib/Screens/AuthScreen/signup_screen.dart
+++ lib/Screens/AuthScreen/signup_screen.dart
@@ -1,16 +1,87 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:todoapp/Config/colors.dart';
 import 'package:todoapp/Screens/widgets/background_widgets.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
 
   @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  @override
   Widget build(BuildContext context) {
+    final TextEditingController emailTEController = TextEditingController();
+    final TextEditingController passwordTEController = TextEditingController();
+
     return Scaffold(
-      body: SafeArea(
-        child: BackgroundWidgets(
-          child: Column(
-            children: [],
+      body: BackgroundWidgets(
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 100),
+                  Text(
+                    "Get Started With",
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const SizedBox(height: 24),
+                  TextFormField(
+                    controller: emailTEController,
+                    decoration: const InputDecoration(
+                      hintText: 'Email',
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: passwordTEController,
+                    decoration: const InputDecoration(
+                      hintText: 'Password',
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Icon(Icons.arrow_circle_right_outlined),
+                  ),
+                  const SizedBox(height: 16),
+                  Center(
+                    child: Column(
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text("Forgot Password?"),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            style: TextStyle(
+                                color: Colors.black.withOpacity(0.8),
+                                fontWeight: FontWeight.w600,
+                                letterSpacing: 0.4),
+                            text: "Don't have and account?",
+                            children: [
+                              TextSpan(
+                                text: " Sign Up",
+                                style: const TextStyle(
+                                  color: AppColors.primaryColor,
+                                ),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {},
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
