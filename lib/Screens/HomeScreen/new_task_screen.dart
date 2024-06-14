bafe4dfb6f4b43d24766c1433b563b8af60f4557
Shougot mollik <shougotmollik@gmail.com>
2024-06-15 00:58:21 +0600

    new task screen

--- lib/Screens/HomeScreen/new_task_screen.dart
+++ lib/Screens/HomeScreen/new_task_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:todoapp/Screens/widgets/profile_appbar.dart';
+import 'package:todoapp/Config/colors.dart';
+import 'package:todoapp/Screens/HomeScreen/add_task_screen.dart';
+import 'package:todoapp/Screens/widgets/task_item.dart';
+import 'package:todoapp/Screens/widgets/task_summery_card.dart';
 
 class NewTaskScreen extends StatefulWidget {
   const NewTaskScreen({super.key});
@@ -12,50 +15,50 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: profileAppBar(),
-      body: const Column(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 8, left: 8, right: 8),
+        child: Column(
+          children: [
+            _buildSummerySection(),
+            const SizedBox(height: 8),
+            Expanded(
+                child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return const TaskItems();
+              },
+            ))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppColors.primaryColor,
+        foregroundColor: Colors.white,
+        onPressed: _onTapAddTaskButton,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget _buildSummerySection() {
+    return const SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
         children: [
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                TaskSummeryCard(taskCount: "09", taskTitle: "Canceled"),
-                TaskSummeryCard(taskCount: "09", taskTitle: "Completed"),
-                TaskSummeryCard(taskCount: "09", taskTitle: "Progress"),
-                TaskSummeryCard(taskCount: "09", taskTitle: "New Task"),
-              ],
-            ),
-          )
+          TaskSummeryCard(taskCount: "09", taskTitle: "Canceled"),
+          TaskSummeryCard(taskCount: "09", taskTitle: "Completed"),
+          TaskSummeryCard(taskCount: "09", taskTitle: "Progress"),
+          TaskSummeryCard(taskCount: "09", taskTitle: "New Task"),
         ],
       ),
     );
   }
-}
 
-class TaskSummeryCard extends StatelessWidget {
-  const TaskSummeryCard({
-    super.key,
-    required this.taskCount,
-    required this.taskTitle,
-  });
-
-  final String taskCount;
-  final String taskTitle;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      color: Colors.white,
-      elevation: 3,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text(taskCount, style: Theme.of(context).textTheme.titleLarge),
-            Text(taskTitle, style: Theme.of(context).textTheme.labelLarge),
-          ],
-        ),
-      ),
-    );
+  void _onTapAddTaskButton() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const AddTaskScreen(),
+        ));
   }
 }
