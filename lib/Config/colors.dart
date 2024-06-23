1daaf8a4f7ea3df3aa24d4d9348b6f00197beaeb
Shougot mollik <shougotmollik@gmail.com>
2024-06-23 20:53:11 +0600

    design part in complete

--- lib/Config/colors.dart
+++ lib/Config/colors.dart
@@ -3,6 +3,6 @@ import 'dart:ui';
 class AppColors {
   AppColors._();
 
-  static const Color primaryColor = Color(0xffFF3D00);
+  static const Color primaryColor = Color(0xff388E3C);
   static const Color onPrimaryColor = Color(0xffffffff);
 }
