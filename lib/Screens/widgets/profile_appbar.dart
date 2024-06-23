7780c3312c9c15b7ebd6c08e558eacb51098d1af
Shougot mollik <shougotmollik@gmail.com>
2024-06-23 20:53:25 +0600

    design part in complete

--- lib/Screens/widgets/profile_appbar.dart
+++ lib/Screens/widgets/profile_appbar.dart
@@ -1,29 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:todoapp/Config/colors.dart';
+import 'package:todoapp/Screens/HomeScreen/update_profile_screen.dart';
 
-AppBar profileAppBar() {
+AppBar profileAppBar(context, [bool fromUpdateProfile = false]) {
   return AppBar(
     backgroundColor: AppColors.primaryColor,
     leading: const Padding(
       padding: EdgeInsets.all(8.0),
       child: CircleAvatar(),
     ),
-    title: const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          "Shougot Mollik",
-          style: TextStyle(color: Colors.white, fontSize: 16),
-        ),
-        Text(
-          "email@gmail.com",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 12,
-            fontWeight: FontWeight.w500,
+    title: GestureDetector(
+      onTap: () {
+        if (fromUpdateProfile) {
+          return;
+        }
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const UpdateProfileScreen(),
+            ));
+      },
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Shougot Mollik",
+            style: TextStyle(color: Colors.white, fontSize: 16),
           ),
-        ),
-      ],
+          Text(
+            "email@gmail.com",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 12,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
     ),
     actions: [
       IconButton(
