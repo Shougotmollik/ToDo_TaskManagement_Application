b787246cd349b1cc4653b564b0c80a2c36ec8968
Shougot mollik <shougotmollik@gmail.com>
2024-07-05 19:15:14 +0600

    network caller

--- lib/data/network_caller/network_caller.dart
+++ lib/data/network_caller/network_caller.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 import 'package:todoapp/data/model/network_response.dart';
 
@@ -32,11 +33,16 @@ class NetworkCaller {
   static Future<NetworkResponse> postRequest(String url,
       {Map<String, dynamic>? body}) async {
     try {
+      debugPrint(url);
+      debugPrint(body.toString());
       Response response = await post(
         Uri.parse(url),
         body: jsonEncode(body),
         headers: {'Content-type': 'Application/json'},
       );
+      debugPrint(response.statusCode.toString());
+      debugPrint(response.body);
+
       if (response.statusCode == 200 || response.statusCode == 201) {
         final decodedData = jsonDecode(response.body);
         return NetworkResponse(
