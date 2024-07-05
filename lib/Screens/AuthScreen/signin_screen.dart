16753eaff20dbd0f237c9c5a2cff3852ff290325
Shougot mollik <shougotmollik@gmail.com>
2024-07-05 19:14:42 +0600

    signin screen

--- lib/Screens/AuthScreen/signin_screen.dart
+++ lib/Screens/AuthScreen/signin_screen.dart
@@ -5,6 +5,11 @@ import 'package:todoapp/Screens/AuthScreen/email_verification_screen.dart';
 import 'package:todoapp/Screens/AuthScreen/signup_screen.dart';
 import 'package:todoapp/Screens/HomeScreen/main_bottom_nav_screen.dart';
 import 'package:todoapp/Screens/widgets/background_widgets.dart';
+import 'package:todoapp/Screens/widgets/snack_bar_message.dart';
+import 'package:todoapp/data/model/network_response.dart';
+import 'package:todoapp/data/network_caller/network_caller.dart';
+import 'package:todoapp/data/utilities/url.dart';
+import 'package:todoapp/utility/app_constants.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -18,6 +23,10 @@ class _SignInScreenState extends State<SignInScreen> {
   final TextEditingController _emailTEController = TextEditingController();
   final TextEditingController _passwordTEController = TextEditingController();
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _showPassword = false;
+  bool _signInProgress = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,71 +35,109 @@ class _SignInScreenState extends State<SignInScreen> {
           child: SingleChildScrollView(
             child: Padding(
               padding: const EdgeInsets.all(24),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 100),
-                  Text(
-                    "Get Started With",
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  const SizedBox(height: 24),
-                  TextFormField(
-                    controller: _emailTEController,
-                    decoration: const InputDecoration(
-                      hintText: 'Email',
-                      hintStyle: TextStyle(
-                        color: Colors.grey,
-                      ),
+              child: Form(
+                key: _formKey,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 100),
+                    Text(
+                      "Get Started With",
+                      style: Theme.of(context).textTheme.titleLarge,
                     ),
-                  ),
-                  const SizedBox(height: 8),
-                  TextFormField(
-                    controller: _passwordTEController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      hintText: 'Password',
-                      hintStyle: TextStyle(
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  ElevatedButton(
-                    onPressed: _onTapHomeScreenButton,
-                    child: const Icon(Icons.arrow_circle_right_outlined),
-                  ),
-                  const SizedBox(height: 16),
-                  Center(
-                    child: Column(
-                      children: [
-                        TextButton(
-                          onPressed: _onTapForgotPasswordButton,
-                          child: const Text("Forgot Password?"),
+                    const SizedBox(height: 24),
+                    TextFormField(
+                      controller: _emailTEController,
+                      decoration: const InputDecoration(
+                        hintText: 'Email',
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
                         ),
-                        RichText(
-                          text: TextSpan(
-                            style: TextStyle(
-                                color: Colors.black.withOpacity(0.8),
-                                fontWeight: FontWeight.w600,
-                                letterSpacing: 0.4),
-                            text: "Don't have and account?",
-                            children: [
-                              TextSpan(
-                                text: " Sign Up",
-                                style: const TextStyle(
-                                  color: AppColors.primaryColor,
-                                ),
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = _onTapSignInButton,
-                              ),
-                            ],
+                      ),
+                      validator: (String? value) {
+                        if (value?.trim().isEmpty ?? true) {
+                          return "Something went wrong";
+                        }
+                        if (AppConstants.emailRegExp.hasMatch(value!) ==
+                            false) {
+                          return "Invalid Email Address";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 8),
+                    TextFormField(
+                      controller: _passwordTEController,
+                      keyboardType: TextInputType.emailAddress,
+                      obscureText: _showPassword == false,
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        hintStyle: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            _showPassword = !_showPassword;
+                            if (mounted) {
+                              setState(() {});
+                            }
+                          },
+                          icon: Icon(_showPassword
+                              ? Icons.visibility_off
+                              : Icons.visibility),
+                        ),
+                      ),
+                      validator: (String? value) {
+                        if (value?.trim().isEmpty ?? true) {
+                          return "Enter Your Password";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    Visibility(
+                      visible: _signInProgress == false,
+                      replacement: const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                      child: ElevatedButton(
+                        onPressed: _onTapHomeScreenButton,
+                        child: const Icon(Icons.arrow_circle_right_outlined),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Center(
+                      child: Column(
+                        children: [
+                          TextButton(
+                            onPressed: _onTapForgotPasswordButton,
+                            child: const Text("Forgot Password?"),
                           ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
+                          RichText(
+                            text: TextSpan(
+                              style: TextStyle(
+                                  color: Colors.black.withOpacity(0.8),
+                                  fontWeight: FontWeight.w600,
+                                  letterSpacing: 0.4),
+                              text: "Don't have and account?",
+                              children: [
+                                TextSpan(
+                                  text: " Sign Up",
+                                  style: const TextStyle(
+                                    color: AppColors.primaryColor,
+                                  ),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = _onTapSignInButton,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
@@ -99,12 +146,42 @@ class _SignInScreenState extends State<SignInScreen> {
     );
   }
 
+  Future<void> _signUp() async {
+    _signInProgress = true;
+    if (mounted) {
+      setState(() {});
+    }
+    Map<String, dynamic> requestData = {
+      "email": _emailTEController.text.trim(),
+      "password": _passwordTEController.text,
+    };
+
+    final NetworkResponse networkResponse =
+        await NetworkCaller.postRequest(AppUrls.loginUrl, body: requestData);
+
+    _signInProgress = false;
+    if (mounted) {
+      setState(() {});
+    }
+
+    if (networkResponse.isSuccess) {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const MainBottomNavScreen(),
+          ));
+    } else {
+      if (mounted) {
+        showSnackBarMessage(context,
+            networkResponse.errorMessage ?? "Invalid Username/password");
+      }
+    }
+  }
+
   void _onTapHomeScreenButton() {
-    Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const MainBottomNavScreen(),
-        ));
+    if (_formKey.currentState!.validate()) {
+      _signUp();
+    }
   }
 
   void _onTapSignInButton() {
