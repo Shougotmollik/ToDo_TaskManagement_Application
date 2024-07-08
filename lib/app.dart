04fbc261abbd01138ffa2fc00b44e2bd1ab7b8d0
Shougot mollik <shougotmollik@gmail.com>
2024-07-08 15:37:54 +0600

    implementation

--- lib/app.dart
+++ lib/app.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:todoapp/Config/theme.dart';
 import 'package:todoapp/Screens/SplashScreen/splash_screen.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: MyApp.navigatorKey,
       themeMode: ThemeMode.light,
       theme: AppThemes.lightTheme,
       home: const SplashScreen(),
