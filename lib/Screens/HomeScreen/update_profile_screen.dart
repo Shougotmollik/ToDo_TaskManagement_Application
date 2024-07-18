f40ba98ceac0d30554f389eeab13eb3ae4af138d
shougotmollik <shougotmollik@gmail.com>
2024-07-18 13:49:22 +0600

    update profile

--- lib/Screens/HomeScreen/update_profile_screen.dart
+++ lib/Screens/HomeScreen/update_profile_screen.dart
@@ -3,6 +3,7 @@ import 'package:todoapp/Config/colors.dart';
 import 'package:todoapp/Screens/widgets/background_widgets.dart';
 import 'package:todoapp/Screens/widgets/profile_appbar.dart';
 import 'package:todoapp/controller/auth_controller.dart';
+import 'package:todoapp/utility/app_constants.dart';
 
 class UpdateProfileScreen extends StatefulWidget {
   const UpdateProfileScreen({super.key});
@@ -18,10 +19,15 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
   final TextEditingController _mobileTEController = TextEditingController();
   final TextEditingController _passwordTEController = TextEditingController();
 
+  final GlobalKey<FormState> _globalKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     final userData = AuthController.userData!;
     _emailTEController.text = userData.email ?? '';
+    _firstNameTEController.text = userData.firstName ?? '';
+    _lastNameTEController.text = userData.lastName ?? '';
+    _mobileTEController.text = userData.mobile ?? '';
 
     super.initState();
   }
@@ -34,65 +40,96 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
           child: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 60),
-              // Text("Update Profile",
-              //     style: Theme.of(context).textTheme.titleLarge),
-              // const SizedBox(height: 16),
-              _buildImagePicker(),
-              const SizedBox(height: 16),
-              TextFormField(
-                controller: _emailTEController,
-                decoration: const InputDecoration(
-                    hintText: "Email",
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                    )),
-              ),
-              const SizedBox(height: 8),
-              TextFormField(
-                controller: _firstNameTEController,
-                decoration: const InputDecoration(
-                    hintText: "First Name",
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                    )),
-              ),
-              const SizedBox(height: 8),
-              TextFormField(
-                controller: _lastNameTEController,
-                decoration: const InputDecoration(
-                    hintText: "Last Name",
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                    )),
-              ),
-              const SizedBox(height: 8),
-              TextFormField(
-                controller: _mobileTEController,
-                decoration: const InputDecoration(
-                    hintText: "Mobile",
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                    )),
-              ),
-              const SizedBox(height: 8),
-              TextFormField(
-                controller: _passwordTEController,
-                decoration: const InputDecoration(
-                    hintText: "Password",
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                    )),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                  onPressed: () {},
-                  child: const Icon(Icons.arrow_circle_right_outlined)),
-              const SizedBox(height: 16),
-            ],
+          child: Form(
+            key: _globalKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 60),
+                // Text("Update Profile",
+                //     style: Theme.of(context).textTheme.titleLarge),
+                // const SizedBox(height: 16),
+                GestureDetector(
+                  onTap: _profileImagePick,
+                  child: _buildImagePicker(),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _emailTEController,
+                  enabled: false,
+                  decoration: const InputDecoration(
+                      hintText: "Email",
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                      )),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: _firstNameTEController,
+                  validator: (String? value) {
+                    if (value?.trim().isEmpty ?? true) {
+                      return 'Enter your first name';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                      hintText: "First Name",
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                      )),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: _lastNameTEController,
+                  validator: (value) {
+                    if (value?.trim().isEmpty ?? true) {
+                      return 'Enter your last name';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                      hintText: "Last Name",
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                      )),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: _mobileTEController,
+                  validator: (String? value) {
+                    if (value?.trim().isEmpty ?? true) {
+                      return 'Enter your mobile number';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                      hintText: "Mobile",
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                      )),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: _passwordTEController,
+                  validator: (String? value) {
+                    if (value?.trim().isEmpty ?? true) {
+                      return 'Enter your password';
+                    }
+                  },
+                  decoration: const InputDecoration(
+                      hintText: "Password",
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                      )),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                    onPressed: () {},
+                    child: const Icon(Icons.arrow_circle_right_outlined)),
+                const SizedBox(height: 16),
+              ],
+            ),
           ),
         ),
       )),
@@ -125,6 +162,8 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
       ),
     );
   }
+
+  Future<void> _profileImagePick() async {}
 
   @override
   void dispose() {
