a7078c22d44659ecf3d8446f353dfce1b6f80ee8
shougotmollik <shougotmollik@gmail.com>
2024-08-08 16:03:22 +0600

    Api integration

--- lib/data/utilities/url.dart
+++ lib/data/utilities/url.dart
@@ -3,5 +3,8 @@ class AppUrls {
   static const String registrationUrl = '$_baseUrl/registration';
   static const String loginUrl = '$_baseUrl/login';
   static const String createTaskUrl = '$_baseUrl/createTask';
-  static const String newTaskUrl = '$_baseUrl/listTaskByStatus/new';
+  static const String newTaskUrl = '$_baseUrl/listTaskByStatus/New';
+  static const String completedTaskUrl = '$_baseUrl/listTaskByStatus/Completed';
+  static const String canceledTaskUrl = '$_baseUrl/listTaskByStatus/Canceled';
+  static const String progressTaskUrl = '$_baseUrl/listTaskByStatus/Progress';
 }
