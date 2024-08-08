a0f50d1501a9c71a4a0b7966ab704683e0f4d030
shougotmollik <shougotmollik@gmail.com>
2024-08-08 16:03:04 +0600

    Api integration

--- lib/Screens/HomeScreen/progress_task_screen.dart
+++ lib/Screens/HomeScreen/progress_task_screen.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/Screens/widgets/centered_progress_indicator.dart';
+import 'package:todoapp/Screens/widgets/snack_bar_message.dart';
+import 'package:todoapp/Screens/widgets/task_item.dart';
+import 'package:todoapp/data/model/network_response.dart';
+import 'package:todoapp/data/model/task_list_wrapper_model.dart';
+import 'package:todoapp/data/model/task_model.dart';
+import 'package:todoapp/data/network_caller/network_caller.dart';
+import 'package:todoapp/data/utilities/url.dart';
 
 class ProgressTaskScreen extends StatefulWidget {
   const ProgressTaskScreen({super.key});
@@ -8,17 +16,63 @@ class ProgressTaskScreen extends StatefulWidget {
 }
 
 class _ProgressTaskScreenState extends State<ProgressTaskScreen> {
+  bool _progressTaskIndicator = false;
+  List<TaskModel> progressTaskList = [];
+
+  @override
+  void initState() {
+    _completedNewTasks();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          return null;
-        
-          // return const TaskItems();
-        },
+      body: Padding(
+        padding: const EdgeInsets.only(top: 8, left: 8, right: 8),
+        child: Column(
+          children: [
+            const SizedBox(height: 8),
+            Expanded(
+                child: Visibility(
+              visible: _progressTaskIndicator == false,
+              replacement: const CenteredProgressIndicator(),
+              child: ListView.builder(
+                itemCount: progressTaskList.length,
+                itemBuilder: (context, index) {
+                  return TaskItems(
+                    taskModel: progressTaskList[index],
+                  );
+                },
+              ),
+            ))
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> _completedNewTasks() async {
+    _progressTaskIndicator = true;
+    if (mounted) {
+      setState(() {});
+    }
+
+    NetworkResponse response =
+        await NetworkCaller.getRequest(AppUrls.progressTaskUrl);
+    if (response.isSuccess) {
+      TaskListWrapperModel taskListWrapperModel =
+          TaskListWrapperModel.fromJson(response.responseData);
+
+      progressTaskList = taskListWrapperModel.list ?? [];
+    } else {
+      if (mounted) {
+        showSnackBarMessage(context, "Get Progress task failed");
+      }
+    }
+    _progressTaskIndicator = false;
+    if (mounted) {
+      setState(() {});
+    }
   }
 }
